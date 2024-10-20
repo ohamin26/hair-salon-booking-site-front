0c01d20984516517c075a33969a397aafc81821b
ohamin26 <ohamin26@gmail.com>
2024-10-20 22:40:25 +0900

    💄 style : 헤어샵 페이지(상세) 캐러셀 반응형 추가

--- src/utils/getSnapPoint.tsx
+++ src/utils/getSnapPoint.tsx
@@ -6,14 +6,20 @@ export const getSnapPoint = (scrollDelta: number, screenWidth: number) => {
     if (scrollDelta < 200) return 1.3;
 
     return 1;
+  } else if (screenWidth < 900) {
+    if (scrollDelta < 50) return 1.95;
+    if (scrollDelta < 100) return 1.7;
+    if (scrollDelta < 150) return 1.4;
+    if (scrollDelta < 200) return 1.2;
+    return 1;
   } else if (screenWidth < 1200) {
-    if (scrollDelta < 50) return 2.05;
-    if (scrollDelta < 100) return 1.5;
+    if (scrollDelta < 50) return 1.64;
+    if (scrollDelta < 100) return 1.4;
     if (scrollDelta < 150) return 1.3;
     if (scrollDelta < 200) return 1.2;
     return 1;
   } else {
-    if (scrollDelta < 50) return 1.5;
+    if (scrollDelta < 50) return 1.65;
     if (scrollDelta < 100) return 1.4;
     if (scrollDelta < 150) return 1.3;
     if (scrollDelta < 200) return 1.2;
