d4f6318d917672abf8a1264888b4538026e77fea
ohamin26 <ohamin26@gmail.com>
2024-10-28 16:25:59 +0900

    🐛 fix : 헤어샵 상세 페이지 홈 컴포넌트 바텀시트 스냅 포인트 기종 별로 추가

--- src/utils/getSnapPoint.tsx
+++ src/utils/getSnapPoint.tsx
@@ -13,22 +13,28 @@ export const getSnapPoint = (scrollDelta: number, screenWidth: number) => {
     if (scrollDelta < 200) return 1.2;
     return 1.035;
   } else if (screenWidth < 800) {
-    if (scrollDelta < 50) return 2.3;
+    if (scrollDelta < 50) return 1.8;
     if (scrollDelta < 100) return 1.5;
     if (scrollDelta < 150) return 1.35;
     if (scrollDelta < 200) return 1.2;
-    return 1.045;
+    return 1.035;
   } else if (screenWidth < 900) {
     if (scrollDelta < 50) return 1.55;
     if (scrollDelta < 100) return 1.4;
     if (scrollDelta < 150) return 1.3;
     if (scrollDelta < 200) return 1.2;
     return 1.035;
-  } else {
-    if (scrollDelta < 50) return 1.75;
+  } else if (screenWidth >= 1550 && screenWidth < 1600) {
+    if (scrollDelta < 50) return 1.73;
     if (scrollDelta < 100) return 1.5;
     if (scrollDelta < 150) return 1.3;
     if (scrollDelta < 200) return 1.2;
     return 1.035;
+  } else {
+    if (scrollDelta < 50) return 2.3;
+    if (scrollDelta < 100) return 1.5;
+    if (scrollDelta < 150) return 1.3;
+    if (scrollDelta < 200) return 1.2;
+    return 1.045;
   }
 };
