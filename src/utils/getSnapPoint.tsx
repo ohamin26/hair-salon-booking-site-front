98ab01f7af3fddfba3a6ee5b1bfa727b6253cb01
ohamin26 <ohamin26@gmail.com>
2024-10-30 23:45:39 +0900

    🐛 fix : 스냅 포인트 변경 로직 리팩토링

--- src/utils/getSnapPoint.tsx
+++ src/utils/getSnapPoint.tsx
@@ -1,46 +1,20 @@
 export const getSnapPoint = (scrollDelta: number, screenWidth: number) => {
-  if (screenWidth < 600) {
-    if (scrollDelta < 50) return 1.93;
-    if (scrollDelta < 100) return 1.7;
-    if (scrollDelta < 150) return 1.5;
-    if (scrollDelta < 200) return 1.2;
+  const snapPoints = [
+    { maxScreenWidth: 600, snapPoint: [1.93, 1.7, 1.5, 1.2, 1.04] },
+    { maxScreenWidth: 700, snapPoint: [1.65, 1.4, 1.3, 1.2, 1.035] },
+    { maxScreenWidth: 800, snapPoint: [1.8, 1.5, 1.35, 1.2, 1.035] },
+    { maxScreenWidth: 900, snapPoint: [1.55, 1.4, 1.3, 1.2, 1.029] },
+    { maxScreenWidth: 1550, snapPoint: [2.3, 1.7, 1.3, 1.2, 1.045] },
+    { maxScreenWidth: 1600, snapPoint: [1.73, 1.5, 1.3, 1.2, 1.035] },
+    { maxScreenWidth: 2000, snapPoint: [1.75, 1.5, 1.3, 1.2, 1.035] },
+    { maxScreenWidth: Infinity, snapPoint: [2.3, 1.5, 1.3, 1.2, 1.045] },
+  ];
 
-    return 1.035;
-  } else if (screenWidth < 700) {
-    if (scrollDelta < 50) return 1.65;
-    if (scrollDelta < 100) return 1.4;
-    if (scrollDelta < 150) return 1.3;
-    if (scrollDelta < 200) return 1.2;
-    return 1.035;
-  } else if (screenWidth < 800) {
-    if (scrollDelta < 50) return 1.8;
-    if (scrollDelta < 100) return 1.5;
-    if (scrollDelta < 150) return 1.35;
-    if (scrollDelta < 200) return 1.2;
-    return 1.035;
-  } else if (screenWidth < 900) {
-    if (scrollDelta < 50) return 1.55;
-    if (scrollDelta < 100) return 1.4;
-    if (scrollDelta < 150) return 1.3;
-    if (scrollDelta < 200) return 1.2;
-    return 1.035;
-  } else if (screenWidth >= 1550 && screenWidth < 1600) {
-    if (scrollDelta < 50) return 1.73;
-    if (scrollDelta < 100) return 1.5;
-    if (scrollDelta < 150) return 1.3;
-    if (scrollDelta < 200) return 1.2;
-    return 1.035;
-  } else if (screenWidth < 2000) {
-    if (scrollDelta < 50) return 1.75;
-    if (scrollDelta < 100) return 1.5;
-    if (scrollDelta < 150) return 1.3;
-    if (scrollDelta < 200) return 1.2;
-    return 1.035;
-  } else {
-    if (scrollDelta < 50) return 2.3;
-    if (scrollDelta < 100) return 1.5;
-    if (scrollDelta < 150) return 1.3;
-    if (scrollDelta < 200) return 1.2;
-    return 1.045;
-  }
+  const scrollRanges = [50, 100, 150, 200];
+
+  const snapPointData = snapPoints.find(({ maxScreenWidth }) => screenWidth < maxScreenWidth);
+  const snapPoint = snapPointData?.snapPoint || [1.04];
+
+  const index = scrollRanges.findIndex((item) => scrollDelta < item);
+  return snapPoint[index === -1 ? snapPoint.length - 1 : index];
 };
